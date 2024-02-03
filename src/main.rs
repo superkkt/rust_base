debe49583d4980b1bce7460a08b5da50f5c299ea
Kitae Kim <superkkt@gmail.com>
2024-02-04 02:42:13 +0900

    caller_name

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use log::error;
 use stderrlog;
+use backtrace::Backtrace;
 
 fn main() {
     init_logger(log::Level::Debug);
@@ -26,10 +27,27 @@ fn init_logger(verbosity: log::Level) {
     //
     // https://docs.rs/log/latest/log/#implementing-a-logger
 
+    if let Some(name) = caller_name() {
+        println!("caller: {name}");
+    }
+
     stderrlog::new()
         .module(module_path!())
         .verbosity(verbosity)
         .timestamp(stderrlog::Timestamp::Millisecond)
         .init()
         .unwrap();
+}
+
+#[inline(never)]
+fn caller_name() -> Option<String> {
+    let backtrace = Backtrace::new();
+    let symbol = backtrace
+        .frames()
+        .iter()
+        .flat_map(|frame| frame.symbols())
+        .nth(7)?;
+    let name = format!("{}", symbol.name()?);
+    let name = name.rsplit_once("::")?.0.to_string();
+    Some(name)
 }
