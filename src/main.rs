ab8553822bca1806238f7136821cbd1c56a42035
Kitae Kim <superkkt@gmail.com>
2024-02-10 00:21:55 +0900

    Implement a simple logger

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,43 @@
-use log::error;
-use stderrlog;
 use backtrace::Backtrace;
+use log::{debug, Level, Log, Metadata, Record};
 
 fn main() {
-    init_logger(log::Level::Debug);
-    error!("Hello, world!");
+    init_logger(log::LevelFilter::Debug);
+    debug!("Hello World!");
 }
 
-fn init_logger(verbosity: log::Level) {
-    // TODO: implement a custom logger to log caller's function name.
-    //
-    // use backtrace::Backtrace;
-    //
-    // #[inline(never)]
-    // fn caller_name() -> Option<String> {
-    //     let backtrace = Backtrace::new();
-    //     let symbol = backtrace
-    //         .frames()
-    //         .iter()
-    //         .flat_map(|frame| frame.symbols())
-    //         .nth(1)?;
-    //     let name = format!("{}", symbol.name()?);
-    //     let name = name.rsplit_once("::")?.0.to_string();
-    //     Some(name)
-    // }
-    //
-    // https://docs.rs/log/latest/log/#implementing-a-logger
+pub struct Logger;
 
-    if let Some(name) = caller_name() {
-        println!("caller: {name}");
+impl Log for Logger {
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        metadata.level() <= Level::Trace
     }
 
-    stderrlog::new()
-        .module(module_path!())
-        .verbosity(verbosity)
-        .timestamp(stderrlog::Timestamp::Millisecond)
-        .init()
-        .unwrap();
+    fn log(&self, record: &Record) {
+        if self.enabled(record.metadata()) == false {
+            return;
+        }
+
+        let timestamp = chrono::Local::now().format("%Y-%m-%d %H:%M:%S%.3f %Z");
+        if let Some(caller) = caller_name() {
+            println!("{}: {}: {}: {}", timestamp, record.level(), caller, record.args());
+        } else {
+            println!("{}: {}: {}", timestamp, record.level(), record.args());
+        }
+    }
+
+    fn flush(&self) {}
+}
+
+fn init_logger(level: log::LevelFilter) {
+    log::set_boxed_logger(Box::new(Logger)).unwrap();
+    log::set_max_level(level);
 }
 
 #[inline(never)]
 fn caller_name() -> Option<String> {
     let backtrace = Backtrace::new();
-    let symbol = backtrace
-        .frames()
-        .iter()
-        .flat_map(|frame| frame.symbols())
-        .nth(7)?;
+    let symbol = backtrace.frames().iter().flat_map(|frame| frame.symbols()).nth(8)?;
     let name = format!("{}", symbol.name()?);
     let name = name.rsplit_once("::")?.0.to_string();
     Some(name)
